8a7db21e4ba942fee5ebe851fdac885f25a09e0b
Joao14m <joaomarsil4@gmail.com>
2025-07-04 16:34:32 -0400

    Navbar completed

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -1,8 +1,36 @@
 const sidebar = document.querySelector(".sidebar"); 
 const sidebarToggler = document.querySelector(".sidebar-toggler");
+const menuToggler = document.querySelector(".menu-toggler");
+
+const collapsedSidebarHeight = "56px";
+const fullSidebarHeight = "calc(100vh - 32px)";
 
 // Toggle sidebar's collapsed state
 sidebarToggler.addEventListener("click", () => {
     // .classList.toggle is a method that adds or removes a CSS class from an element
     sidebar.classList.toggle("collapsed");
 });
+
+
+// Update sidebar height and menu toggle text
+const toggleMenu = (isMenuActive) => {
+    sidebar.style.height = isMenuActive ? 
+    `${sidebar.scrollHeight}px` : collapsedSidebarHeight ;
+    menuToggler.querySelector("span").innerText = isMenuActive ? 
+    "close" : "menu";
+}
+
+// Toggle menu-active class and adjust height
+menuToggler.addEventListener("click", () => {
+    toggleMenu(sidebar.classList.toggle("menu-active"));
+});
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth >= 1024) {
+        sidebar.style.height = fullSidebarHeight;
+    } else {
+        sidebar.classList.remove("collapsed");
+        sidebar.style.height = "auto";
+        toggleMenu(sidebar.classList.contains("menu-active"));
+    }
+})
